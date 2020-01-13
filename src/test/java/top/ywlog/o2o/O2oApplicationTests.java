db37469e1e8168e0638cab2704dde532677109dd
Durian <1323807496@qq.com>
2020-01-13 16:10:27 +0800

    校园商铺springboot版本

---
+++ src/test/java/top/ywlog/o2o/O2oApplicationTests.java
@@ -0,0 +1,15 @@
+package top.ywlog.o2o;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class O2oApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
